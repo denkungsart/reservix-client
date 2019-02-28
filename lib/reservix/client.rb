9674e5526e1f08c59f022aa5e3e9e2a2a1ad2ed1
Evgeny Sugakov <shiroginne@gmail.com>
2019-02-28 12:52:44 +0100

    Hello world 🎊

---
+++ lib/reservix/client.rb
@@ -0,0 +1,8 @@
+require "reservix/client/version"
+
+module Reservix
+  module Client
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/reservix/client_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Reservix::Client do
+  it "has a version number" do
+    expect(Reservix::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
