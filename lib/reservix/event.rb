d7c067703a2b54ef1b3d18df1086bc890edd97f6
Evgeny Sugakov <shiroginne@gmail.com>
2019-03-08 17:06:47 +0100

    Add methods #starts_at and #ends_at for Event
    To have proper, parsed time not a strings returned from the API

--- lib/reservix/event.rb
+++ lib/reservix/event.rb
@@ -2,6 +2,14 @@ module Reservix
   class Event < OpenStruct
     include Referenceable
 
+    def starts_at
+      DateTime.parse("#{startdate} #{starttime}")
+    end
+
+    def ends_at
+      DateTime.parse("#{enddate} #{endtime}") if enddate
+    end
+
     def event_group
       EventGroup.new(fetch_reference('eventgroup'))
     end
